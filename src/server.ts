799687a4e034566402aed113b7e8745c7c39a14b
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-07-24 16:58:37 -0300

    registration of appointments

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import express, { request, response } from 'express';
-import routes from './routes';
+import routes from './routes/index';
 
 const app = express();
 app.use(express.json());
 
-app.post('/', (request, response) => {
-  const { name, email } = request.body;
-
-  response.json({ message: 'Hello' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
