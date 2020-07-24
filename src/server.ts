4fe419ec66073e1cb60e2d3d9c2c91ef9310c810
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-07-24 14:47:36 -0300

    standardizing code with eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.get('/', (request, response) => {
-  response.json({ message: 'Hello World!'});
+  response.json({ message: `"Hello World!"` });
 });
 
 app.listen(3333, () => {
