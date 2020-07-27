00f863f9acaca1d889cf061e0839945b9cf445ac
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-07-27 13:38:55 -0300

    listing appointments

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -8,6 +8,12 @@ class AppointmentsRepository {
     this.appointments = [];
   }
 
+  public all(): Appointment[] {
+    const { appointments } = this;
+
+    return appointments;
+  }
+
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
       isEqual(appointment.date, date),
