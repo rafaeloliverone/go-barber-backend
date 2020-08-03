c24f9be4103b13b61a3505e6d8561cf8354649ab
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-08-03 15:38:29 -0300

    updating avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,7 +2,11 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: tmpFolder,
+
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'tmp'),
     filename(request, file, callback) {
